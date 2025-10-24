f24e1ec58387e37aabf66ea67426e8a85bbd7ca7
henryoriondev <henryorion.dev@gmail.com>
2025-10-24 01:05:05 -0300

    1 semana - criando formulario

--- processa.php
+++ processa.php
@@ -3,10 +3,10 @@
 $nome = $_POST['nome'];
 $idade = $_POST['idade'];
 
-// Valida e mostra mensagem
+//   Valida e mostra mensagem
 if ($idade >= 18) {
     echo "Olá, $nome! Você tem $idade anos e é maior de idade.";
 } else {
     echo "Olá, $nome! Você tem $idade anos e é menor de idade.";
 }
-?>
+?> 
